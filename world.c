1984054849515d1723deb48f3ac7ac06bcf1515a
Heikki Kallasjoki <fis+github@zem.fi>
2011-01-03 19:20:53 +0200

    Ignore even more truncated chunk-updates

--- world.c
+++ world.c
@@ -104,8 +104,9 @@ static void handle_chunk(int x0, int y0, int z0,
 	int zbuf_len = (sizeof zbuf) - zstr.avail_out;
 	inflateEnd(&zstr);
 
-	if (zbuf_len != (5*xs*ys*zs+1)/2)
-		stopf("broken decompressed chunk length: %d != %d", (int)zbuf_len, (int)(5*xs*ys*zs+1)/2);
+	if (zbuf_len < xs*ys*zs)
+		stopf("broken decompressed chunk length: %d != %d and < %d",
+		      (int)zbuf_len, (int)(5*xs*ys*zs+1)/2, xs*ys*zs);
 
 	struct coord current_chunk = { .x = -0x80000000, .z = -0x80000000 };
 	struct chunk *c = 0;
