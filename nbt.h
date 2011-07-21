fed16b734cbf562ed305a2a348ebf9fb43d9a1a8
Heikki Kallasjoki <fis+github@zem.fi>
2011-07-21 17:06:53 +0300

    A header is fine too.

--- nbt.h
+++ nbt.h
@@ -31,6 +31,10 @@ struct nbt_tag *nbt_new_struct(char *name);
 
 void nbt_free(gpointer tag);
 
+jint nbt_blob_len(struct nbt_tag *s);
+unsigned char *nbt_blob_data(struct nbt_tag *s);
+
+struct nbt_tag *nbt_struct_field(struct nbt_tag *s, const char *name);
 void nbt_struct_add(struct nbt_tag *s, struct nbt_tag *field);
 
 unsigned char *nbt_compress(struct nbt_tag *tag, unsigned *len);
