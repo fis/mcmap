4666a139da29df96726b6583e29ed0efe535f6f9
Elliott Hird <penguinofthegods@gmail.com>
2011-07-25 16:55:54 +0100

    Apologia for windows.h

--- windows.h
+++ windows.h
@@ -1,3 +1,8 @@
+/* This is a huge hack to get around the fact that libraries include
+   windows.h, and windows.h defines some short identifiers that we
+   use, and I'm too stubborn to change those identifiers.
+  
+   I wonder if my pain will ever end. */
 #undef tell
 #ifdef RGB
 #define RGB_WAS_DEFINED
