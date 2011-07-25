17b6de8699c1573b32e1b124006a997ad2399838
Elliott Hird <penguinofthegods@gmail.com>
2011-07-25 16:51:58 +0100

    windows.h fixes

--- windows.h
+++ windows.h
@@ -1,5 +1,13 @@
 #undef tell
+#ifdef RGB
+#define RGB_WAS_DEFINED
 #pragma push_macro("RGB")
+#endif
 #include_next <windows.h>
 #define tell mctell
+#ifdef RGB_WAS_DEFINED
 #pragma pop_macro("RGB")
+#undef RGB_WAS_DEFINED
+#else
+#undef RGB
+#endif
