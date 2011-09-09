e21fe8cdb5e77b677737a6b857cce619cce3cafd
Elliott Hird <penguinofthegods@gmail.com>
2011-09-10 00:32:34 +0100

    Create a smob for packet_t at startup

--- scheme.c
+++ scheme.c
@@ -1,8 +1,36 @@
+#include <glib.h>
 #include <libguile.h>
 
+#include "protocol.h"
 #include "scheme.h"
+
+static size_t smob_packet_free(SCM packet_smob)
+{
+	packet_t *p = (packet_t *) SCM_SMOB_DATA(packet_smob);
+	packet_free(p);
+	return 0;
+}
+
+static int smob_packet_print(SCM packet_smob, SCM port, scm_print_state *pstate)
+{
+	/* TODO: Better printing function */
+	packet_t *p = (packet_t *) SCM_SMOB_DATA(packet_smob);
+	scm_puts("#<packet ", port);
+	scm_display(scm_from_uint(p->type), port);
+	scm_puts(">", port);
+	return 1;
+}
+
+static SCM smob_packet_equalp(SCM packet_smob_a, SCM packet_smob_b)
+{
+	/* TODO: Better equality function */
+	return scm_eq_p(packet_smob_a, packet_smob_b);
+}
 
 void init_scheme()
 {
+	packet_tag = scm_make_smob_type("packet", sizeof(packet_t));
+	scm_set_smob_free(packet_tag, smob_packet_free);
+	scm_set_smob_print(packet_tag, smob_packet_print);
+	scm_set_smob_equalp(packet_tag, smob_packet_equalp);
 }
-
--- scheme.h
+++ scheme.h
@@ -1,6 +1,8 @@
 #ifndef MCMAP_SCHEME_H
 #define MCMAP_SCHEME_H
 
+scm_t_bits packet_tag;
+
 void init_scheme(void);
 
 #endif /* MCMAP_SCHEME_H */
