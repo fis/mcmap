3a895a4750cb0a159585ea2b9f08df1defeafea3
Elliott Hird <penguinofthegods@gmail.com>
2011-09-10 03:44:31 +0100

    Add incomplete make-packet function

--- scheme.c
+++ scheme.c
@@ -4,7 +4,8 @@
 #include "protocol.h"
 #include "scheme.h"
 
-SCM packet_type_table;
+static SCM packet_type_to_symbol;
+static SCM symbol_to_packet_type;
 
 SCM_SMOB(scm_tc16_packet_type, "packet", sizeof(packet_t));
 
@@ -35,22 +36,43 @@ SCM make_packet_smob(packet_t *p)
 	SCM_RETURN_NEWSMOB(scm_tc16_packet_type, packet_dup(p));
 }
 
+SCM_DEFINE(scheme_make_packet, "make-packet", 1, 0, 0, (SCM type_symbol),
+	"Return the type of the packet.")
+#define FUNC_NAME "make-packet"
+{
+	SCM_VALIDATE_SYMBOL(1, type_symbol);
+
+	SCM type = scm_hash_ref(symbol_to_packet_type, type_symbol, SCM_BOOL_F);
+	if (scm_is_eq(type, SCM_BOOL_F))
+		scm_out_of_range(FUNC_NAME, type_symbol);
+
+	packet_constructor_t pc = packet_create(scm_to_uint(type));
+	SCM_RETURN_NEWSMOB(scm_tc16_packet_type, packet_construct(&pc));
+}
+#undef FUNC_NAME
+
 SCM_DEFINE(scheme_packet_type, "packet-type", 1, 0, 0, (SCM packet_smob),
 	"Return the type of the packet.")
 #define FUNC_NAME "packet-type"
 {
 	SCM_VALIDATE_SMOB(1, packet_smob, packet_type);
 	packet_t *p = (packet_t *) SCM_SMOB_DATA(packet_smob);
-	return scm_hash_ref(packet_type_table, scm_from_uint(p->type), SCM_UNDEFINED);
+	return scm_hash_ref(packet_type_to_symbol, scm_from_uint(p->type), SCM_UNDEFINED);
 }
 #undef FUNC_NAME
 
 void init_scheme()
 {
-	packet_type_table = scm_permanent_object(scm_make_hash_table(SCM_UNDEFINED));
+	packet_type_to_symbol = scm_permanent_object(scm_make_hash_table(SCM_UNDEFINED));
+	symbol_to_packet_type = scm_permanent_object(scm_make_hash_table(SCM_UNDEFINED));
 
+	SCM packet_id;
+	SCM packet_scmname;
 	#define PACKET(id, cname, scmname, nfields, ...) \
-		scm_hash_set_x(packet_type_table, scm_from_uint(id), scm_from_locale_symbol(scmname));
+		packet_id = scm_from_uint(id); \
+		packet_scmname = scm_from_locale_symbol(scmname); \
+		scm_hash_set_x(packet_type_to_symbol, packet_id, packet_scmname); \
+		scm_hash_set_x(symbol_to_packet_type, packet_scmname, packet_id);
 	#include "protocol.x"
 	#undef PACKET
 
