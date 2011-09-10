9b4aed38bf995227f6ad74e15dc1088ed102b017
Elliott Hird <penguinofthegods@gmail.com>
2011-09-10 09:26:48 +0100

    Actually use init_filename

--- main.c
+++ main.c
@@ -63,7 +63,7 @@ void *real_main(void *data)
 	char *init_filename = g_strconcat(g_get_home_dir(), "/.mcmap/init.scm", NULL);
 	if (g_file_test(init_filename, G_FILE_TEST_EXISTS))
 	{
-		scm_c_primitive_load(g_strconcat(g_get_home_dir(), "/.mcmap/init.scm", NULL));
+		scm_c_primitive_load(init_filename);
 	}
 	g_free(init_filename);
 
