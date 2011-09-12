05c4bd8ff5001fdc0207f4292c763d39fc5904a7
Elliott Hird <penguinofthegods@gmail.com>
2011-09-12 11:11:48 +0100

    Add a somewhat imperfect on-packet macro

--- scheme/init.scm
+++ scheme/init.scm
@@ -3,3 +3,12 @@
 
 (define (chat str)
   (packet-inject 'server (make-packet 'chat #:message str)))
+
+(define-syntax on-packet
+  (syntax-rules ()
+    ((on-packet origin (type binding) body ...)
+     (add-hook!
+      (packet-hook 'type)
+      (lambda (from binding)
+        (if (or (eq? 'origin 'any) (eq? from 'origin))
+            (begin body ...)))))))
