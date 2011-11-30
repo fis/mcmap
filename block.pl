1a91d6dc29026a299552229f4ff627b598d1e341
Elliott Hird <penguinofthegods@gmail.com>
2011-11-30 09:06:32 +0000

    Generate block names from Minecraft Wiki info
    Like the protocol-scraping stuff, but simpler. Default block colours are
    now in colors.c. As an added bonus, "make block" prints out the missing
    entries in that file.

---
+++ block.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+my %types = (
+	'water' => 'LIQUID',
+	'lava' => 'LIQUID',
+);
+
+my %traits = (
+	'water' => 'WATER',
+	'lava' => 'LAVA',
+);
+
+my %names = ('air' => 1);
+
+print <<EOF;
+#include <glib.h>
+
+#include "block.h"
+
+struct block_info block_info[256] = {
+	[0x00] = {"air", AIR, NO_TRAIT},
+EOF
+
+while (my $line = <>)
+{
+	$line =~ /^\| height="27px" \| .*? \|\| (.*?) \|\| .*? \|\| (.*)$/ or next;
+	my $id = $1;
+	my $name = lc $2;
+	$id =~ s/.*>(.+)<.*/$1/;
+	$name =~ s/<sup>.*//;
+	$name =~ s/\[\[(.*?\|)?(.*?)\]\]/$2/g;
+	$name =~ s/"(.*?)" state/$1/;
+	$name =~ s/block of (.*)/$1 block/;
+	$name =~ s/brick block/brick/;
+	$name =~ s/wooden plank/wood/;
+	$name =~ s/glowing redstone ore/redstone ore/;
+	$name =~ s/wall sign/sign/;
+	$name =~ s/stationary (.*)/$1/;
+	$name =~ s/ *$//;
+	$names{$name} = 1;
+	printf "\t[0x%02x] = {\"%s\", %s, %s_TRAIT},\n",
+		$id, $name, $types{$name} || 'SOLID',
+		$traits{$name} || 'NO';
+}
+
+print "};\n";
+
+# bonus: check colors.c for completeness
+
+my %colored;
+open my $colors, '<', 'colors.c' or die $!;
+while (<$colors>)
+{
+	next unless /^\t"(.*):/;
+	if (!$names{$1})
+	{
+		print STDERR "colors.c: invalid block: $1\n";
+	}
+	$colored{$1} = 1;
+}
+foreach my $name (keys %names)
+{
+	unless ($colored{$name})
+	{
+		print STDERR "colors.c: missing color: $name\n";
+	}
+}
