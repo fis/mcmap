2483f574e74c41ad3ee43ed367f4be6ea846ba55
Elliott Hird <penguinofthegods@gmail.com>
2011-12-14 12:20:18 +0000

    Add altitude control to cross-section mode

--- map_cross.c
+++ map_cross.c
@@ -5,30 +5,79 @@
 #include "protocol.h"
 #include "map.h"
 
+struct state
+{
+	bool follow_y;
+	jint y;
+};
+
+static void update_player_pos(void *data);
+
 static char *describe(void *data, GPtrArray *attribs)
 {
+	struct state *state = data;
+	if (state->follow_y) g_ptr_array_add(attribs, "follow");
 	return "cross-section";
+}
+
+static bool update_alt(struct state *state, jint y)
+{
+	if (y < 0)
+		y = 0;
+	else if (y >= CHUNK_YSIZE)
+		y = CHUNK_YSIZE - 1;
+
+	if (y != state->y)
+	{
+		state->y = y;
+		map_update_all();
+		return true;
+	}
+
+	return false;
 }
 
 static bool handle_key(void *data, SDL_KeyboardEvent *e)
 {
-	return false;
+	struct state *state = data;
+
+	switch (e->keysym.unicode)
+	{
+	case 'f':
+		state->follow_y ^= true;
+		update_player_pos(data);
+		map_mode_changed();
+		return false;
+	}
+
+	switch (e->keysym.sym)
+	{
+	case SDLK_UP:
+		return update_alt(state, state->y + 1);
+
+	case SDLK_DOWN:
+		return update_alt(state, state->y - 1);
+
+	default:
+		return false;
+	}
 }
 
 static void update_player_pos(void *data)
 {
-	map_update_all();
+	struct state *state = data;
+
+	if (state->follow_y)
+	{
+		update_alt(state, player_pos.y);
+		map_repaint();
+	}
 }
 
 static jint mapped_y(void *data, struct chunk *c, unsigned char *b, jint bx, jint bz)
 {
-	jint y = player_pos.y;
-	if (y < 0)
-		return 0;
-	else if (y >= CHUNK_YSIZE)
-		return CHUNK_YSIZE - 1;
-	else
-		return y;
+	struct state *state = data;
+	return state->y;
 }
 
 static rgba_t block_color(void *data, struct chunk *c, unsigned char *b, jint bx, jint bz, jint y)
@@ -38,8 +87,12 @@ static rgba_t block_color(void *data, struct chunk *c, unsigned char *b, jint bx
 
 struct map_mode *map_init_cross_mode()
 {
+	struct state *state = g_new(struct state, 1);
+	state->follow_y = true;
+	state->y = 0;
+
 	struct flat_mode flat_mode;
-	flat_mode.data = NULL;
+	flat_mode.data = state;
 	flat_mode.describe = describe;
 	flat_mode.handle_key = handle_key;
 	flat_mode.update_player_pos = update_player_pos;
